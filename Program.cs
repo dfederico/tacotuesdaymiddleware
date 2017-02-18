a6b6c0fd44d7b1389c00335f59f5bb5c7b7af27d
dfederico <dfederico7@gmail.com>
2017-02-18 09:52:15 -0600

    Reverting to a web project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ConsoleApplication
+namespace WebApplication
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
