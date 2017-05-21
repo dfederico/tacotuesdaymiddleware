c94c8494df31d5d0a8b554ec82c7c947163dd57c
Uriah Blatherwick <uriahb@gmail.com>
2017-05-21 09:56:03 -0500

    add a button!

--- TacoTuesday.web/app/app.component.ts
+++ TacoTuesday.web/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from "@angular/core";
+import * as buttonModule from "tns-core-modules/ui/button";
+import * as bindable from "tns-core-modules/ui/core/bindable";
+import * as observable from "tns-core-modules/data/observable";
 
 @Component({
   selector: "my-app",
   template: `
     <ActionBar title="Taco Tuesday" class="action-bar"></ActionBar>
+    <StackLayout orientation="vertical">
+    
+    <Button text="blah">clicky</Button>
     <Image src="~/images/apple.jpg"></Image>
+    </StackLayout>
   `,
   styles: [`
     @keyframes spin {
