8f5fa01a8b261119f24ba4607ba7e21fa6644d3a
Uriah Blatherwick <uriahb@gmail.com>
2017-06-04 09:58:06 -0500

    getting real actual results and stuff

---
+++ Models/YelpBusiness.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TacoTuesday.Models
+{
+    public class YelpBusiness
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Services/LocationService.cs
+++ Services/LocationService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
 using TacoTuesday.Models;
@@ -19,25 +20,31 @@ namespace TacoTuesday.Services
         }
         public List<Location> Get(decimal longitude, decimal latitude)
         {
+            var results = new List<Location>();
             //1. Ensusre that we have an access token
             var token = _authTokenService.EnsureAuthToken();
             //2. Call the yelp api Search
 
             var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
-            HttpResponseMessage result = httpClient.GetAsync("https://api.yelp.com/v3/businesses/search?term=taco&latitude=44.977753&longitude=-99.265011").Result;
+            HttpResponseMessage apiResult = httpClient.GetAsync("https://api.yelp.com/v3/businesses/search?term=taco&latitude=44.976456&longitude=-93.270791").Result;
 
-            if (result.IsSuccessStatusCode)
+            if (apiResult.IsSuccessStatusCode)
             {
                 var locations = 
-                result.Content.ReadAsStringAsync();
+                apiResult.Content.ReadAsStringAsync();
+
+                var yelpResult = JsonConvert.DeserializeObject<YelpResult>(apiResult.Content.ReadAsStringAsync().Result);
+                foreach (var business in yelpResult.Businesses)
+                {
+                    results.Add(new Location
+                    {
+                        LocationName = business.Name
+                    });
+                }
             }
             //3. Return model for results
-            return new List<Location>{
-                new Location{
-                    LocationName = ""
-                }
-            };
+            return results;
         }
     }
 }
