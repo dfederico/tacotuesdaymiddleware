0b8de83126ad0440b6522e1f8ec3b90965fd722b
Uriah Blatherwick <uriahb@gmail.com>
2017-07-30 08:14:45 -0500

    runnable app... for now

--- TacoTuesday.web/app/app.component.ts
+++ TacoTuesday.web/app/app.component.ts
@@ -10,18 +10,10 @@ import * as observable from "tns-core-modules/data/observable";
     <StackLayout orientation="vertical">
     
     <Button text="blah">clicky</Button>
-    <Image src="~/images/apple.jpg"></Image>
+    
     </StackLayout>
   `,
   styles: [`
-    @keyframes spin {
-      from { transform: rotate(0); } to { transform: rotate(360); }
-    }
-    Image {
-      animation-name: spin; animation-duration: 3s;
-      animation-iteration-count: infinite;
-      animation-timing-function: linear;
-    }
   `]
 })
 export class AppComponent {}
